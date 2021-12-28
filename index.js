5bbaeb88a8e92967a927102cc45057ff009b7084
Eli Pagan <mbk8689@gmail.com>
2021-12-28 12:01:28 -0500

    added function for choices

--- index.js
+++ index.js
@@ -2,6 +2,24 @@ var inquirer = require('inquirer');
 const cTable = require('console.table');
 const connection = require('./db/connection');
 
+const loadMainMenu = () => {
+    inquirer
+     .prompt([
+         {
+         type: "rawlist",
+         name: "choices",
+         message: "What would ypu like to do?",
+         choices: [
+             "View all departments",
+             "View all roles",
+             "View all employees",
+             "Add a department",
+             "Add a role",
+             "Add an employee",
+             "Update employee role"
+         ]
+     }
+ ])
 
 
 
